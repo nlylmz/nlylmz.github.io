ae0c2c98d71005ba5d15176c2fe012cd8da5eb95
nlylmz <nyilmaz@sas.upenn.edu>
2025-12-08 23:05:01 +0000

    Deploying to gh-pages from @ nlylmz/nlylmz.github.io@00f91f5458c08424c44b100165fb88c97e628d79 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "nlylmz/nilayyilmaz.github.io",
+    "data-repo": "nlylmz/nlylmz.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
